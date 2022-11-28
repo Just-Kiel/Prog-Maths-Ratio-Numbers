09166843545586c3895c536b89cdb90de68f0f9e
Aurore <82760936+Just-Kiel@users.noreply.github.com>
2022-11-28 14:54:08 +0100

    :fire: Forgot to delete gcd code by myself

--- unitTests/src/side_functions.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/side_functions.hpp"
-
-int gcd(int a, int b) // The function runs recursive in nature to return GCD  
-{  
-    if (b == 0) // if b becomes 0 return a   
-        return a;  
-    return gcd(b, a % b); // divide to a by b to make smaller number   
-       
-}  
